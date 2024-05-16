e8489286313d9700ea44a6cb8e104b28dd646e19
Simuratli Eljan <simuratli@gmail.com>
2024-05-16 12:40:15 +0400

    bg

--- app/layout.js
+++ app/layout.js
@@ -13,6 +13,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <meta property="og:image" content="/bg.png>" />
+      <meta property="og:image:type" content="/bg.png>" />
+      <meta property="og:image:width" content="/bg.png>" />
+      <meta property="og:image:height" content="/bg.png>" />
       <body>{children}
       <script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.6.347/pdf.min.js" integrity="sha512-Z8CqofpIcnJN80feS2uccz+pXWgZzeKxDsDNMD/dJ6997/LSRY+W4NmEt9acwR+Gt9OHN0kkI1CTianCwoqcjQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
       </body>
